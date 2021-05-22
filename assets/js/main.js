fd4f096c7cf9fd0ed9983ed76a385b3fa2430015
Your Name <squirtmissle123@outlook.com>
2021-05-22 16:47:14 -0400

    owl

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,15 +169,7 @@
     delay: 10,
     time: 1000
   });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
+  
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
@@ -198,14 +190,6 @@
     $(document).ready(function() {
       $('.venobox').venobox();
     });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
   });
 
   // Init AOS
